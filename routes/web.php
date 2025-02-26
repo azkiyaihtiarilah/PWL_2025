2c73db65cf6f06cc54509368c10a182c47d2049b
= <=>
2025-02-26 21:16:45 +0700

    Practicum Steps Optional Parameters

--- routes/web.php
+++ routes/web.php
@@ -21,36 +21,44 @@ Route::get('/', function () {
    return view ('welcome');
 });
 
-Route::get('hello', function () {
-    return ('Hello World');
- });
+// Route::get('hello', function () {
+//     return ('Hello World');
+//  });
 
-Route::get('/world', function () {
-    return ('World');
-});
+// Route::get('/world', function () {
+//     return ('World');
+// });
 
-Route::get('/', function () {
-    return ('Welcome');
-});
+// Route::get('/', function () {
+//     return ('Welcome');
+// });
 
-Route::get('/about/name', function () {
-    return ('Hello my Name is Azkiya, and my nim is 2341760175');
-});
+// Route::get('/about/name', function () {
+//     return ('Hello my Name is Azkiya, and my nim is 2341760175');
+// });
 
-Route::get('/user/{name}/{nim}', function ($name,$nim) {
-    return ('My name is '.$name.' and my student number is '.$nim);
-});
+// Route::get('/user/{name}/{nim}', function ($name,$nim) {
+//     return ('My name is '.$name.' and my student number is '.$nim);
+// });
 
-Route::get('/user/{Azkiya}', function ($name) { 
+// Route::get('/user/{Azkiya}', function ($name) { 
+//     return 'My name is '.$name; 
+// }); 
+
+// Route::get('/posts/{post}/comments/{comment}', function 
+// ($postId, $commentId) { 
+//     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; 
+// });
+
+// Route::get('/articles/{id}', function 
+// ($id) { 
+//     return 'Halaman Artikel dengan ID '.$id; 
+// });
+
+Route::get('/user/{name?}', function ($name=null) { 
     return 'My name is '.$name; 
 }); 
 
-Route::get('/posts/{post}/comments/{comment}', function 
-($postId, $commentId) { 
-    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; 
-});
-
-Route::get('/articles/{id}', function 
-($id) { 
-    return 'Halaman Artikel dengan ID '.$id; 
+Route::get('/user/{name?}', function ($name='John') { 
+    return 'Nama saya '.$name;
 });
